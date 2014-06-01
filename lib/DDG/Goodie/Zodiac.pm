052ae106b9495963ac33998067ee19b8c70fea4d
NOMADY <nomady@zoho.com>
2014-06-01 17:13:11 +0530

    Removed the Redundant White Spaces

--- lib/DDG/Goodie/Zodiac.pm
+++ lib/DDG/Goodie/Zodiac.pm
@@ -15,18 +15,18 @@ use DateTime::Event::Zodiac qw(zodiac_date_name);
 
 zci answer_type => "zodiac";
 
+triggers startend	=> "zodiac","zodiac sign,""starsign","star sign";
+
 #File MetaData
-primary_example_queries 	"zodiac 21st June";
-secondary_example_queries 	"31 Dec starsign","1st Jan 1981 star sign","zodiac sign 1 Nov";
-description					"Find the Zodiac Sign for the Respective Date given as Input based on Tropical Zodiac(2011) Source : https://en.wikipedia.org/wiki/Zodiac";
-name						"zodiac";
-category					"special";
-topics						"science";
-attribution					email		=> 'nomady@zoho.com',
-							github 		=> ['https://github.com/n0mady','NOMADY'];
-triggers					startend	=> "zodiac","zodiac sign,""starsign","star sign";
+primary_example_queries "zodiac 21st June";
+secondary_example_queries "31 Dec starsign","1st Jan 1981 star sign","zodiac sign 1 Nov";
+description	"Find the Zodiac Sign for the Respective Date given as Input based on Tropical Zodiac(2011) Source : https://en.wikipedia.org/wiki/Zodiac";
+name "zodiac";
+category "special";
+topics "science";
+attribution	email		=> 'nomady@zoho.com',
+			github 		=> ['https://github.com/n0mady','NOMADY'];
 			
-			 
 handle remainder => sub {
 	
 		#User Entered Date/Query
